5e14a252351803b038985f51792524e4707b216f
Andrew <gfstels@yandex.ru>
2025-02-16 18:01:30 +0500

    new

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import {useLocation} from 'react-router-dom'
 
 import Form from './components/Form'
 
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 
 import './App.scss'
 import './progressbar.scss'
@@ -49,86 +49,86 @@ function App() {
     const fbAds = params.get('fb')
 
     return (
-        <BrowserRouter>
-            <div className="App">
-                <div className="quiz-container">
-                    <div className="progress-bar">
-                        <p className="progress-bar__number">Шаг {currentStep + 1}</p>
 
-                        <div className="progress-bar__quiz-steps">
-                            {quizSteps.map((_, index) => (
-                                <div key={index}
-                                     className={`progress-circle ${index <= currentStep ? 'progress-circle--active' : ''}`}></div>
-                            ))}
-                        </div>
+        <div className="App">
+            <div className="quiz-container">
+                <div className="progress-bar">
+                    <p className="progress-bar__number">Шаг {currentStep + 1}</p>
+
+                    <div className="progress-bar__quiz-steps">
+                        {quizSteps.map((_, index) => (
+                            <div key={index}
+                                 className={`progress-circle ${index <= currentStep ? 'progress-circle--active' : ''}`}></div>
+                        ))}
                     </div>
+                </div>
 
-                    {currentStep < quizSteps.length ? (
-                        <>
-                            {/* <CookieConsent /> */}
-                            <div className="question-container">
-                                <div className="question-container__number">{currentStep + 1}</div>
-                                <h3>{quizSteps[currentStep].question}</h3>
+                {currentStep < quizSteps.length ? (
+                    <>
+                        {/* <CookieConsent /> */}
+                        <div className="question-container">
+                            <div className="question-container__number">{currentStep + 1}</div>
+                            <h3>{quizSteps[currentStep].question}</h3>
 
-                                {currentStep > 0 && (
-                                    <div onClick={() => setCurrentStep(currentStep - 1)} className="back-button">
-                                        Назад
-                                    </div>
-                                )}
-                            </div>
+                            {currentStep > 0 && (
+                                <div onClick={() => setCurrentStep(currentStep - 1)} className="back-button">
+                                    Назад
+                                </div>
+                            )}
+                        </div>
 
 
-                            <div className="options-container">
-                                {quizSteps[currentStep].options.map((option, index) => {
+                        <div className="options-container">
+                            {quizSteps[currentStep].options.map((option, index) => {
 
-                                    if (quizSteps[currentStep].type === 'text') {
-                                        // console.log('123')
-                                        return (
-                                            <div
-                                                key={index}
-                                                className={`option-button ${answers[currentStep] === option.text ? 'option-button--active' : ''}`}
-                                                onClick={() => handleOptionClick(option)}
-                                            >
-                                                <div className="option-text">{option.text}</div>
-                                            </div>
-                                        )
-                                    }
-
+                                if (quizSteps[currentStep].type === 'text') {
+                                    // console.log('123')
                                     return (
-
                                         <div
                                             key={index}
                                             className={`option-button ${answers[currentStep] === option.text ? 'option-button--active' : ''}`}
                                             onClick={() => handleOptionClick(option)}
                                         >
-
-
-                                            <img src={option.image} alt={option.text} className="option-image"/>
-
                                             <div className="option-text">{option.text}</div>
-                                        </div>)
-                                })}
-                            </div>
+                                        </div>
+                                    )
+                                }
 
-                            <div onClick={handleNextClick}
-                                 className={`next-button ${!answers[currentStep] ? 'next-button--inactive' : ''}`}>
-                                Дальше
-                            </div>
-                        </>
-                    ) : (
-                        <>
-                            <Form
-                                answers={answers}
-                                setCurrentStep={setCurrentStep}
-                                setAnswers={setAnswers}
-                                fbc={fbclid ? fbclid : ''}
-                                browserName={userAgent}
-                            />
-                        </>
-                    )}
-                </div>
+                                return (
+
+                                    <div
+                                        key={index}
+                                        className={`option-button ${answers[currentStep] === option.text ? 'option-button--active' : ''}`}
+                                        onClick={() => handleOptionClick(option)}
+                                    >
+
+
+                                        <img src={option.image} alt={option.text} className="option-image"/>
+
+                                        <div className="option-text">{option.text}</div>
+                                    </div>)
+                            })}
+                        </div>
+
+                        <div onClick={handleNextClick}
+                             className={`next-button ${!answers[currentStep] ? 'next-button--inactive' : ''}`}>
+                            Дальше
+                        </div>
+                    </>
+                ) : (
+                    <>
+                        <Form
+                            answers={answers}
+                            setCurrentStep={setCurrentStep}
+                            setAnswers={setAnswers}
+                            fbc={fbclid ? fbclid : ''}
+                            browserName={userAgent}
+                        />
+                    </>
+                )}
             </div>
-        </BrowserRouter>
+        </div>
+
     )
 }
 
