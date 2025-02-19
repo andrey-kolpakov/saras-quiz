e62260d2ef61e99eb9507281bb9ceeee9cfbdc9a
Andrew <gfstels@yandex.ru>
2025-02-19 23:44:45 +0500

    new

--- src/utils/handleAmoWebHook.js
+++ src/utils/handleAmoWebHook.js
@@ -9,17 +9,17 @@ export default function handleAmoWebhook(name, phone, text) {
 
     // console.log(payload)
 
-    // axios
-    //     .post('https://saras-amo.netlify.app/.netlify/functions/check', payload, {
-    //         headers: {
-    //             'Content-Type': 'application/json',
-    //         },
-    //     })
-    //     .then((response) => {
-    //         console.log('Успешная отправка данных на вебхук:', response.data)
-    //
-    //     })
-    //     .catch((error) => {
-    //         console.error('Ошибка при отправке данных на вебхук:', error)
-    //     })
+    axios
+        .post('https://saras-amo.netlify.app/.netlify/functions/check', payload, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        .then((response) => {
+            console.log('Успешная отправка данных на вебхук:', response.data)
+
+        })
+        .catch((error) => {
+            console.error('Ошибка при отправке данных на вебхук:', error)
+        })
 }
